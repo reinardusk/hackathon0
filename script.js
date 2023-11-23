b916fe552b829c1a669406cb4790fe0050da7aa6
unknown <reinardusk@gmail.com>
2023-11-24 03:17:05 +0700

    script, html

--- script.js
+++ script.js
@@ -11,54 +11,56 @@ let database = [
         star: 3,
         imgPath: "./img/people/cewe 1.jpg",
         description: "haii guys",
-        price: 2
+        price: 4
     }, 
     {
         id: 2,
         name: "Claire",
         age: "dewasa",
         gender: "female",
-        star: "5.00",
+        star: 5,
         imgPath: "./img/people/cewe 2.jpg",
-        description: "haii guys",
+        description: "#nohashtag",
         price: 3
     }, 
     {
         id: 3,
         name: "Rias",
         age: "remaja",
-        gender: "female",
+        gender: 4,
         star: "5.00",
         imgPath: "./img/people/cewe 3.jpg",
-        description: "haii guys",
-        price: 2
+        description: "muka-muka orang bener",
+        price: 4
     }, 
     {
         id: 4,
         name: "Malice",
         age: "remaja",
         gender: "female",
-        star: "5.00",
+        star: 5,
         imgPath: "./img/people/cewe 4.jpg",
-        description: "haii guys",
-        price: 1
+        description: "gadis ketek",
+        price: 3
     }, 
     {
         id: 5,
         name: "Ume",
         age: "remaja",
+        star: 8,
         gender: "female",
         imgPath: "./img/people/cewe 5.jpg",
-        description: "haii guys",
-        price: 5
+        description: "kyaaaa~! ovO",
+        price: 322
     }, 
     {
         id: 6,
         name: "Magenta",
         age: "remaja",
         gender: "female",
+        star: 4,
         imgPath: "./img/people/cewe 6.jpg",
-        description: "haii guys",
+        description: "kyunnn~! gOpay",
         price: 2.5
     }, 
     {
@@ -66,17 +68,19 @@ let database = [
         name: "Tarno",
         age: "remaja",
         gender: "male",
+        star: 3,
         imgPath: "./img/people/cowo 1.jpg",
-        description: "haii guys",
-        price: 1
+        description: "bukan sulap..",
+        price: 2
     }, 
     {
         id: 8,
         name: "Dika",
         age: "dewasa",
         gender: "male",
+        star: 3,
         imgPath: "./img/people/cowo 2.jpg",
-        description: "haii guys",
+        description: "bukan she/her",
         price: 1
     }, 
     {
@@ -84,15 +88,26 @@ let database = [
         name: "Gaga",
         age: "remaja",
         gender: "male",
+        star: 4,
         imgPath: "./img/people/cowo 3.jpg",
-        description: "haii guys",
-        price: 1
+        description: "ga kuat~",
+        price: 2
     }
 ]
 let section = document.querySelector("#product1");
 let container = document.querySelector(".pro-container");
-for (let i = 0; i < database.length; i++) {
+// aaaaaaaaaaaaa
+let card = document.createElement("div");
+let dbImage = document.createElement("img");
+let dbDescription = document.createElement("div");
 
+let descriptionPrice = document.createElement("h4");
+let descriptionText = document.createElement("span");
+let starContainer = document.createElement("h5");
+let modulPayment = document.querySelector("#payment")
+let imagePayment = document.createElement("img");
+// aaaaaaaaaaaaa
+for (let i = 0; i < database.length; i++) {
     let card = document.createElement("div");
     card.classList.add("pro");
     let dbImage = document.createElement("img");
@@ -101,7 +116,7 @@ for (let i = 0; i < database.length; i++) {
     let dbDescription = document.createElement("div");
     dbDescription.classList.add("description");
     let dbName = document.createElement("h6");
-    dbName.innerText = `Name: ${database[i].name}`;
+    dbName.innerText = `${database[i].name}`;
     // star
     let starContainer = document.createElement("h5");
     for (let j = 0; j < database[i].star; j++) {
@@ -115,16 +130,18 @@ for (let i = 0; i < database.length; i++) {
     
     // price
     let descriptionPrice = document.createElement("h4");
-    descriptionPrice.innerText = `$ ${database[i].price}`;
+    descriptionPrice.innerText = `$ ${database[i].price} / hour`;
     
     // heart
     let heartContainer = document.createElement("div");
     let heartButton = document.createElement("a");
-    heartButton.setAttribute("href", "#")
+    // heartButton.setAttribute("href", "")
     let heartIcon = document.createElement("i");
-    
+    // -----
     heartIcon.classList.add("fa-solid", "fa-heart", "fa-beat");
+    heartIcon.classList.add("buttonHeart")
     heartButton.append(heartIcon);
+    // ------
     //  append
     dbDescription.append(dbName);
     dbDescription.append(starContainer);
@@ -134,13 +151,105 @@ for (let i = 0; i < database.length; i++) {
     card.append(dbImage);
     card.append(dbDescription);
     card.append(heartContainer);
-    container.append(card);
+    container.append(card);  
+    
+    let modulPayment = document.querySelector("#payment")
+    let paymentContainer = document.querySelector(".payment-container")
+    heartIcon.addEventListener("click", () =>{
+        if (modulPayment.style.display === "flex") {
+            // imagePayment.removeAttribute("img");
+        } else {
+
+            let namePayment = document.createElement("h1");
+            namePayment.classList.add("payTitle");
+            namePayment.innerText = database[i].name;
+            paymentContainer.append(namePayment);
+            let imagePayment = document.createElement("img");
+            modulPayment.style.display="flex";
+            // console.log(1);
+            imagePayment.classList.add("imagePayment");
+            imagePayment.setAttribute("src", database[i].imgPath);
+    
+            let buttonAdd = document.createElement("a");
+            buttonAdd.setAttribute("type", "button");
+            buttonAdd.setAttribute("value", "add");
+            buttonAdd.classList.add("add-btn");
+            buttonAdd.innerText = "+";
+            
+            let buttonRed = document.createElement("a");
+            buttonRed.setAttribute("type", "button");
+            buttonRed.setAttribute("value", "reduce");
+            buttonRed.classList.add("red-btn");
+            buttonRed.innerText = "-";
+    
+            let cost = document.createElement("h4");
+            cost.classList.add("cost");
+            cost.innerText = `$ ${database[i].price}`;
+            
+            let totalHours = 1;
+    
+            let hours = document.createElement("h4");
+            hours.classList.add("hours");
+            hours.innerText = `${totalHours} jam`;
+            
+            modulPayment.append(imagePayment);
+            modulPayment.append(cost);
+            modulPayment.append(hours);
+            modulPayment.append(buttonAdd);
+            modulPayment.append(buttonRed);
+            
+            let multiplier = database[i].price
+            
+            
+            buttonAdd.addEventListener("click", () => {
+                cost.innerText = `$ ${database[i].price += multiplier}`
+                totalHours++
+                hours.innerText = `${totalHours} jam`;
+                
+            })
+            buttonRed.addEventListener("click", () => {
+                cost.innerText = `$ ${database[i].price -= multiplier}`
+                totalHours --
+                hours.innerText = `${totalHours} jam`;
+                if (totalHours == 0 || totalHours < 0) {
+                    totalHours = 0;
+                    hours.innerText = `${totalHours} jam`;
+                }
+                if (database[i].price == 0 || database[i].price < 0) {
+                    database[i].price = 0
+                    cost.innerText = 0;
+                }
+                // console.log(totalHours)
+                // cost.innerText = Math.max(0, database[i].price);
+            })
+    
+    
+            let payButton = document.querySelector(".payButton");
+            payButton.addEventListener(`click`, () => {
+                // modulPayment.append(cost);
+                let panggilan;
+                if (database[i].gender === "female") {
+                    panggilan = "Mbak"
+                }
+                if (database[i].gender === "male") {
+                    panggilan = "Mas"
+                }
+                alert(`Terima kasih telah menggunakan jasa kami. \nKamu akan dicintai oleh ${panggilan} ${database[i].name} sesuai durasi selama ${totalHours} jam. \nTotal biaya yang telah kamu bayarkan ${cost.innerText}. \nSilahkan menunggu beberapa saat untuk mendapatkan DM dari ${panggilan} ${database[i].name}.`);
+                location.reload()
+            })
+            
+            // console.log(imagePayment)
+            // let paymentContainer = document.querySelector(".payment-container");
+            let xButton = document.querySelector(".xButton");
+            xButton.addEventListener("click", () => {
+                // console.log(imagePayment);
+                modulPayment.style.display = "none";
+                imagePayment.remove();
+                cost.remove();
+                namePayment.remove();
+                hours.remove();
+                location.reload()
+            })
+        }
+    })
 }
-
-
-
-
-
-
-
-
